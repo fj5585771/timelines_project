197d9e387a2ccbd545d042f7748f48a89b3b3a50
Fraser Jamieson <fraserjamieson.93@gmail.com>
2021-06-20 15:38:35 +0100

    reset maven

--- server/src/main/java/com/codeclan/timelines/TimelinesApplication.java
+++ server/src/main/java/com/codeclan/timelines/TimelinesApplication.java
@@ -3,6 +3,7 @@ package com.codeclan.timelines;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TimelinesApplication {
 
